e550da35898315c2277040890e13b13a48fc4ced
Jonathan Cheng <jcheng461@gmail.com>
2023-08-11 09:57:29 -0700

    WIP: set up business logic branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 // Business Logic
 
-// function languageDeterminer() {
-
-// }
+function languageDeterminer(difficulty, adjective, color, personality) {
+  let userScore = 0;
+  if (difficulty === "easy") {
+    userScore += 0;
+  } else if (difficulty === "low") {
+    userScore += 1;
+  } else if (difficulty === "medium") {
+    userScore += 2;
+  } else if (difficulty === "high") {
+    userScore += 3;
+  }
+  return userScore;
+}
 
 // User Interface Logic
 function resultPreparer(event) {
   event.preventDefault();
+
+  const userEnteredName = document.querySelector("input#user-name").value;
 }
 
 function formLoader() {

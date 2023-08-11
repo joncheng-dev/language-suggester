b2d4f9e1ac4f301b2934bf1be55d605e6be54e0d
Jonathan Cheng <jcheng461@gmail.com>
2023-08-11 10:59:45 -0700

    add to logic branching in business logic; refactor form

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 // Save for later
 // , adjective, color, personality
-function languageDeterminer(difficulty) {
+function languageDeterminer(difficulty, adjective, color, personality) {
   let userScore = 0;
   if (difficulty === "easy") {
     userScore += 0;
@@ -14,20 +14,41 @@ function languageDeterminer(difficulty) {
     userScore += 3;
   }
 
+  if (adjective === "general") {
+    userScore += 1;
+  } else if (adjective === "niche") {
+    userScore += 2;
+  }
+
+  if (color === "dark") {
+    userScore += 1;
+  } else if (color === "bright") {
+    userScore += 2;
+  }
+
+  if (personality === "extrovert") {
+    userScore += 1;
+  } else if (personality === "introvert") {
+    userScore += 2;
+  }
   return userScore;
 }
 
 // User Interface Logic
 function resultPreparer(event) {
   event.preventDefault();
-
-  const userEnteredName = document.getElementById("user-name").value;
-  const userEnteredDifficulty = document.getElementById("user-difficulty").value;
-
-  let result = languageDeterminer(userEnteredDifficulty);
-  document.getElementById("results-user-name").innerText = userEnteredName;
+  // Collects user entered data
+  const userName = document.getElementById("user-name").value;
+  const userDifficulty = document.getElementById("user-difficulty").value;
+  const userAdjective = document.getElementById("user-adjective").value;
+  const userColor = document.querySelector("input[name='user-color-preference']:checked").value;
+  const userPersonality = document.querySelector("input[name='user-personality']:checked").value;
+  // Calls language determiner function
+  let result = languageDeterminer(userDifficulty, userAdjective, userColor, userPersonality);
+  // Displays results to DOM
+  document.getElementById("results-user-name").innerText = userName;
   document.getElementById("results").innerText = result;
-
+  // Displays Results Div
   document.getElementById("resultsDiv").removeAttribute("class");
 }
 
